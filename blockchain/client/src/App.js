b26e59ae39018b034204ae55a206ee53b7063ffe
Abdul <abdullahmd326@gmail.com>
2022-03-11 23:26:54 -0500

    Reconfigured react folder with client folder of truffle client

--- blockchain/client/src/App.js
+++ blockchain/client/src/App.js
@@ -1,73 +1,38 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../src/Component/Navbar/navbar';
+import Footer from '../src/Component/Footer/footer';
+import HomePage from '../src/Component/HomePage/homepage';
+import Login from '../src/Component/Login/login';
+import SignUp from '../src/Component/SignUp/signup';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductGrid from './Component/ProductGrid/productgrid';
+import { Provider } from 'react-redux';
+import { store }  from './redux/store/store';
+import OrderCheckout from './Component/OrderCheckout/ordercheckout';
+import OrderHistory from './Component/OrderHistory/orderhistory';
 
-import "./App.css";
+const App = () => {
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+  return (
+        <Provider store={store} >
+          <Router>
+            <div className="App">
+              <Navbar />
+                <Routes>
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/productPricing" element={<ProductGrid />} />
+                  <Route path="/orderHistory" element={<OrderHistory />} />
+                  <Route path="/checkout" element={<OrderCheckout />} />
+                  <Route path="/" element={<HomePage />} />
+                </Routes>
+              <Footer />
+            </div>
+          </Router>
+        </Provider>
       );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
   }
-}
 
 export default App;
